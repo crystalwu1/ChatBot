4dd87a49b876d378bdbb52cb52e2d69b40c2b2bc
Crystal Wu <cw683@cornell.edu>
2020-07-30 02:38:47 +0000

    magic number up to 99

--- magic4.py
+++ magic4.py
@@ -1,41 +1,23 @@
+def tensToString(num):
+  tens = ['', '', 'twen', 'thir', 'four', 'fif', 'six', 'seven', 'eigh', 'nine']
+  return tens[num]
+
 def numberToString(num):
+  ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen']
   if num < 0:
     print("invalid number")
     return None
-  elif num == 1:
-    return 'one'
-  elif num == 2:
-    return 'two'
-  elif num == 3:
-    return 'three'
-  elif num == 4:
-    return 'four'
-  elif num == 5:
-    return "five"
-  elif num == 6:
-    return "six"
-  elif num == 7:
-    return 'seven'
-  elif num == 8:
-    return "eight"
-  elif num == 9:
-    return 'nine'
-  elif num == 10:
-    return "ten"
-  # elif num == 11:
-  #   return "eleven"
-  # elif num == 12:
-  #   return "twelve"
-  # elif num == 13:
-  #   return "thirteen"
-  # elif num == 15:
-  #   return "fifteen"
-  # elif num % 10 == 0:
-  #   return numberToString(num//10) + "ty"
-  # else:
-  #   return numberToString(num//10) + "ty" + numberToString(num%10)
+  elif num >= 20:
+    return tensToString(num//10) + 'ty' + numberToString(num%10)
+  elif num > 15:
+    return numberToString(num%10) + 'teen'
+  else:
+    return ones[num]
+
+  
 
 def magicNumber(number):
+  # print(numberToString(number))
   print(number, "is", len(numberToString(number)))
   if len(numberToString(number)) == 4:
     print("4 is the magic number.")
--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import greet
 import unitconversion
 import magic4
 
-
 print("Hello!")
 user = input("What would you like to do today? ")
 name = None
 
 while user != "user" or user != "no":
 
-  # try:
+  try:
 
     words = user.split(" ")
 
@@ -24,7 +23,7 @@ while user != "user" or user != "no":
       unitconversion.convertMetric(words[2], words[4], words[1])
 
     elif words[0].lower() + words[1].lower() == "magicnumber":
-      num = input("Choose an integer from 1 to 10: ")
+      num = input("Choose an integer from 1 to 99: ")
       magic4.magicNumber(int(num))
 
     else:
@@ -33,8 +32,8 @@ while user != "user" or user != "no":
     print("")
     user = input("Anything else? ")
 
-  # except:
-  #   print("")
-  #   user = input("Invalid input. Try again: ")
+  except:
+    print("")
+    user = input("Invalid input. Try again: ")
 
 print("Goodbye!")
